4336eb3029ece85abe62fac25d2bcee85d077c0b
Collin Jilbert <cjilbert504@gmail.com>
2020-05-20 11:16:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById("submit").addEventListener("click", function(event) {
+    event.preventDefault();
+    const text = document.getElementById("new-task-description").value;
+    const tag = document.createElement("li");
+    const element = document.getElementById("tasks");
+    
+    tag.innerHTML = text;
+    element.appendChild(tag);
+    text = "";
+  });
 });
+
+
