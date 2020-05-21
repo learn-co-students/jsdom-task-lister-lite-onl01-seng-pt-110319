f7d3d32347b414c9b7dd884f4529e2477d9aeac7
Collin Jilbert <cjilbert504@gmail.com>
2020-05-20 19:54:18 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  document.getElementById("submit").addEventListener("click", function(event) {
+    const tags = document.getElementsByTagName("input");
+    tags[1].addEventListener("click", function(event) {
     event.preventDefault();
     const text = document.getElementById("new-task-description");
     const tag = document.createElement("li");
