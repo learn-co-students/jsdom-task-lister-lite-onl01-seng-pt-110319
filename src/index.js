bf7a367a659879ec2848d96e5f0b3e2833b15b2d
Jordan <jordanleelong5@gmail.com>
2020-05-22 22:15:05 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   // your code here
+  
+  const submit = document.getElementsByTagName("input");
+  
+  
+  
+  submit[1].addEventListener("click", function(event) {
+    event.preventDefault()
+    const element = document.getElementById('new-task-description');
+    const list = document.createElement('li');
+    const taskList = document.getElementById('tasks');
+
+    list.innerText = element.value;
+    taskList.appendChild(list);
+    element.value = "";
+  });
+
+
 });
+
+
