d96f8b8d4cee80ef6b3458837c49ef674ed29242
nicksolony <nicksolony@gmail.com>
2020-06-13 20:47:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let newTaskButton= document.getElementById('create-new-task');
+  let tasks=document.getElementById("tasks");
+  let counter=0;
+  let sortHi=document.getElementById("sort_hi")
+  let sortLo=document.getElementById("sort_lo")
+  let taskList=tasks.getElementsByTagName("li")
+
+
+
+  function addNewTask() {
+    let newTaskDescription=document.getElementById("new-task-description");
+    let newTaskPriority=document.getElementById("priority");
+    if (newTaskDescription.value !== "") {
+    let newTask = document.createElement("li");
+    newTask.setAttribute("id",`task-${counter}`)
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText= "x";
+    newTask.innerText=`${newTaskDescription.value} `;
+    switch(newTaskPriority.value) {
+      case "High":
+      newTask.style.color="red";
+      break;
+      case "Medium":
+      newTask.style.color="orange";
+      break;
+      case "Low":
+      newTask.style.color="green";
+      break;
+      default:
+    }
+    newTask.appendChild(deleteButton);
+    tasks.appendChild(newTask);
+    counter++;
+    newTaskDescription.value="";
+    newTaskPriority.value="Select priority";
+    deleteButton.addEventListener("click",function(){
+      this.parentNode.remove()
+    })
+    }
+  }
+
+
+  newTaskButton.addEventListener("click",function(event) {
+    addNewTask();
+    event.preventDefault();
+  })
+
+ sortHi.addEventListener("click",function () {
+
+ })
+
+
 });
