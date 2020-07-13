887516e6a7d16137a155a6f96cec488517429f07
github username <github email address>
2020-07-13 19:30:38 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+  const taskForm=document.querySelector("#create-task-form");
+  const tasks=document.querySelector("#tasks")
+ 
+  taskForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    const newTask=document.querySelector("#new-task-description").value
+    
+    tasks.innerHTML+=`
+    <li>${newTask}
+    <button data-action="delete"> X</button>
+    </li>`
+    // const taskCreate=document.createElement("li")
+  
+    // taskCreate.innerText=newTask
+    // tasks.appendChild(taskCreate)
+    taskForm.reset()
+
+  });
+
+  tasks.addEventListener("click", function(event){
+      if (event.target.dataset.action==="delete"){
+        event.target.parentElement.remove()
+      }
+  });
